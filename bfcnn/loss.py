97ba0f5e0cbbf0d0ff155abc940c102a4e7b6b99
Nikolas Markou <nikolasmarkou@gmail.com>
2021-09-28 17:06:33 +0300

    [wip] testing different losses

--- bfcnn/loss.py
+++ bfcnn/loss.py
@@ -48,10 +48,10 @@ def loss_function_builder(
         mean_relative_error_loss = 0.0
         if input_batch is not None and prediction_batch is not None:
             diff = tf.abs(input_batch - prediction_batch)
-            diff_mean = tf.reduce_mean(diff, axis=[1, 2, 3])
             diff_max = tf.reduce_max(diff, axis=[1, 2, 3])
+            diff_div_max = tf.reduce_mean(diff / (diff_max + 1.0), axis=[1, 2, 3])
             mean_relative_error_loss = \
-                tf.reduce_mean(255.0 * diff_mean / (diff_max + 0.0001), axis=[0])
+                tf.reduce_mean(diff_div_max, axis=[0])
 
         # --- mean absolute error from noisy
         mean_absolute_error_noise = 0.0
