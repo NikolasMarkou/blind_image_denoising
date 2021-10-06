e60472934e6a5e25abffc19ad5c186ef121696c6
Nikolas Markou <nikolasmarkou@gmail.com>
2021-10-06 20:59:13 +0300

    [wip]

--- bfcnn/utilities.py
+++ bfcnn/utilities.py
@@ -658,17 +658,15 @@ def build_sparse_resnet_mean_sigma_model(
 
     # --- add base layer
     x = model_input
-    x = keras.layers.Conv2D(**base_conv_params)(x)
     _, sigma = mean_sigma_local(x, kernel_size=(5, 5))
     x = keras.layers.Concatenate()([x, sigma])
+    x = keras.layers.Conv2D(**base_conv_params)(x)
 
     # --- add resnet layers
     for i in range(no_layers):
         previous_layer = x
         x = conv2d_sparse(x, **sparse_conv_params)
         x = keras.layers.Conv2D(**conv_params)(x)
-        _, sigma = mean_sigma_local(x, kernel_size=(5, 5))
-        x = keras.layers.Concatenate()([x, sigma])
         x = keras.layers.Add()([previous_layer, x])
 
     # --- output to original channels
