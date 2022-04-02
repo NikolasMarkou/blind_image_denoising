0973ea8707762ed175e54a6df5ea23c0d2278eb9
Nikolas Markou <nikolasmarkou@gmail.com>
2022-04-02 20:54:58 +0300

    [wip]

--- bfcnn/custom_layers.py
+++ bfcnn/custom_layers.py
@@ -28,44 +28,28 @@ class TrainableMultiplier(tf.keras.layers.Layer):
             name=name,
             **kwargs)
         self._w0 = None
-        self._w1 = None
-        self._activation_w1 = None
-        self._multiplier = multiplier
         self._activation = activation
-        self._weight_regularizer = regularizers.get(regularizer)
+        self._regularizer = regularizers.get(regularizer)
 
     def build(self, input_shape):
         def init_w0_fn(shape, dtype):
             return np.ones(shape, dtype=np.float32) * self._multiplier
-
-        def init_w1_fn(shape, dtype):
-            return np.zeros(shape, dtype=np.float32)
-
         self._w0 = \
             self.add_weight(
-                name="constant",
                 shape=[1],
-                regularizer=None,
-                initializer=init_w0_fn,
-                trainable=False)
-        self._w1 = \
-            self.add_weight(
+                trainable=True,
                 name="multiplier",
-                shape=[1],
-                regularizer=self._weight_regularizer,
-                initializer=init_w1_fn,
-                trainable=self.trainable)
+                initializer=init_w0_fn,
+                regularizer=self._regularizer)
         self._activation = keras.layers.Activation(self._activation)
         super(TrainableMultiplier, self).build(input_shape)
 
     def call(self, inputs):
-        return self._activation(
-            (self._w0 * (1.0 + self._w1)) * inputs)
+        return self._activation(self._w0 * inputs)
 
     def get_config(self):
         return {
-            "constant": self._w0.numpy(),
-            "multiplier": self._w1.numpy(),
+            "multiplier": self._w0.numpy(),
             "activation": self._activation
         }
 
