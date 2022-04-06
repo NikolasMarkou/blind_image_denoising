258f695d6775da70ba733f39c55b62cedd0bb0f0
Nikolas Markou <nikolasmarkou@gmail.com>
2022-04-06 11:51:23 +0300

    [wip] added noise mixer estimation

---
+++ bfcnn/model_noise_estimation.py
@@ -0,0 +1,114 @@
+# ---------------------------------------------------------------------
+
+__author__ = "Nikolas Markou"
+__version__ = "1.0.0"
+__license__ = "MIT"
+
+# ---------------------------------------------------------------------
+
+import abc
+import tensorflow as tf
+from tensorflow import keras
+
+# ---------------------------------------------------------------------
+# local imports
+# ---------------------------------------------------------------------
+
+from .utilities import *
+from .custom_logger import logger
+
+# ---------------------------------------------------------------------
+
+
+def model_builder(
+        config: Dict) -> keras.Model:
+    """
+    Reads a configuration and returns a model
+
+    :param config: configuration dictionary
+    :return: noise estimation model
+    """
+    logger.info("building model with config [{0}]".format(config))
+
+    # --- argument parsing
+    filters = config.get("filters", 32)
+    no_layers = config.get("no_layers", 5)
+    batchnorm = config.get("batchnorm", True)
+    kernel_size = config.get("kernel_size", 3)
+    activation = config.get("activation", "relu")
+    input_shape = config.get("input_shape", (None, None, 3))
+    final_activation = config.get("final_activation", "linear")
+    kernel_regularizer = config.get("kernel_regularizer", "l1")
+    kernel_initializer = config.get("kernel_initializer", "glorot_normal")
+
+    # --- build denoise model
+    model_params = dict(
+        add_gates=False,
+        add_var=False,
+        filters=filters,
+        use_bn=batchnorm,
+        add_sparsity=False,
+        no_layers=no_layers,
+        activation=activation,
+        input_dims=input_shape,
+        kernel_size=kernel_size,
+        final_activation=final_activation,
+        kernel_regularizer=kernel_regularizer,
+        kernel_initializer=kernel_initializer,
+        add_skip_with_input=False,
+        add_projection_to_input=False,
+        add_intermediate_results=False,
+        add_learnable_multiplier=False
+    )
+
+    # --- connect the parts of the model
+    # setup input
+    input_layer = \
+        keras.Input(
+            shape=input_shape,
+            name="input_tensor")
+    x = input_layer
+
+    resnet_model = \
+        build_model_resnet(
+            name="resnet_noise_estimation",
+            **model_params)
+
+    x = resnet_model(x)
+
+    x = keras.layers.GlobalAveragePooling2D()(x)
+
+    x = keras.layers.Flatten()(x)
+
+    output_layer = \
+        keras.layers.Dense(
+            units=1,
+            activation=final_activation)(x)
+
+    # --- wrap and name model
+    model_noise_estimation = \
+        keras.Model(
+            inputs=input_layer,
+            outputs=output_layer,
+            name=f"noise_estimation")
+
+    return model_noise_estimation
+
+# ---------------------------------------------------------------------
+
+
+def noise_estimation_mixer(
+        model_noise_estimation: keras.Model,
+        x0_input_layer,
+        x1_input_layer):
+    x0_noise = model_noise_estimation(x0_input_layer)
+    x1_noise = model_noise_estimation(x1_input_layer)
+    x = keras.layers.Concatenate(axis=-1)([x0_noise, x1_noise])
+    x = keras.layers.Softmax(axis=-1)(x)
+    x0_result, x1_result = tf.unstack(x, axis=-1)
+    return keras.layers.Add()([
+        x0_result * x0_input_layer,
+        x1_result * x1_input_layer])
+
+# ---------------------------------------------------------------------
+
--- bfcnn/utilities.py
+++ bfcnn/utilities.py
@@ -746,5 +746,240 @@ def convnext_blocks(
         # skip connection
         x = keras.layers.Add()([x, previous_layer])
     return x
+
 # ---------------------------------------------------------------------
 
+
+def build_model_resnet(
+        input_dims,
+        no_layers: int,
+        kernel_size: int,
+        filters: int,
+        activation: str = "relu",
+        final_activation: str = "linear",
+        use_bn: bool = True,
+        use_bias: bool = False,
+        kernel_regularizer="l1",
+        kernel_initializer="glorot_normal",
+        channel_index: int = 2,
+        add_skip_with_input: bool = True,
+        add_sparsity: bool = False,
+        add_gates: bool = False,
+        add_var: bool = False,
+        add_intermediate_results: bool = False,
+        add_learnable_multiplier: bool = False,
+        add_projection_to_input: bool = True,
+        name="resnet") -> keras.Model:
+    """
+    builds a resnet model
+
+    :param input_dims: Models input dimensions
+    :param no_layers: Number of resnet layers
+    :param kernel_size: kernel size of the conv layers
+    :param filters: number of filters per convolutional layer
+    :param activation: intermediate activation
+    :param final_activation: activation of the final layer
+    :param channel_index: Index of the channel in dimensions
+    :param use_bn: Use Batch Normalization
+    :param use_bias: use bias
+    :param kernel_regularizer: Kernel weight regularizer
+    :param kernel_initializer: Kernel weight initializer
+    :param add_skip_with_input: if true skip with input
+    :param add_sparsity: if true add sparsity layer
+    :param add_gates: if true add gate layer
+    :param add_var: if true add variance for each block
+    :param add_intermediate_results: if true output results before projection
+    :param add_learnable_multiplier:
+    :param add_projection_to_input: if true project to input tensor channel number
+    :param name: name of the model
+    :return: resnet model
+    """
+    # --- setup parameters
+    bn_params = dict(
+        center=use_bias,
+        scale=True,
+        momentum=0.999,
+        epsilon=1e-4
+    )
+
+    # this make it 68% sparse
+    sparse_params = dict(
+        symmetric=True,
+        max_value=3.0,
+        threshold_sigma=1.0,
+        per_channel_sparsity=False
+    )
+
+    base_conv_params = dict(
+        filters=filters,
+        strides=(1, 1),
+        padding="same",
+        use_bias=use_bias,
+        activation=activation,
+        kernel_size=kernel_size,
+        kernel_regularizer=kernel_regularizer,
+        kernel_initializer=kernel_initializer
+    )
+
+    gate_params = dict(
+        kernel_size=1,
+        filters=filters,
+        strides=(1, 1),
+        padding="same",
+        use_bias=use_bias,
+        activation=activation,
+        kernel_regularizer=kernel_regularizer,
+        kernel_initializer=kernel_initializer
+    )
+
+    first_conv_params = dict(
+        kernel_size=1,
+        filters=filters,
+        strides=(1, 1),
+        padding="same",
+        use_bias=use_bias,
+        activation=activation,
+        kernel_regularizer=kernel_regularizer,
+        kernel_initializer=kernel_initializer,
+    )
+
+    second_conv_params = dict(
+        kernel_size=3,
+        filters=filters * 2,
+        strides=(1, 1),
+        padding="same",
+        use_bias=use_bias,
+        activation=activation,
+        kernel_regularizer=kernel_regularizer,
+        kernel_initializer=kernel_initializer
+    )
+
+    third_conv_params = dict(
+        groups=2,
+        kernel_size=1,
+        filters=filters,
+        strides=(1, 1),
+        padding="same",
+        use_bias=use_bias,
+        activation="linear",
+        kernel_regularizer=kernel_regularizer,
+        kernel_initializer=kernel_initializer
+    )
+
+    final_conv_params = dict(
+        kernel_size=1,
+        strides=(1, 1),
+        padding="same",
+        use_bias=use_bias,
+        activation="linear",
+        filters=input_dims[channel_index],
+        kernel_regularizer=kernel_regularizer,
+        kernel_initializer=kernel_initializer
+    )
+
+    multiplier_params = dict(
+        multiplier=1.0,
+        trainable=True,
+        regularizer="l1",
+        activation="linear"
+    )
+
+    resnet_params = dict(
+        no_layers=no_layers,
+        bn_params=bn_params,
+        first_conv_params=first_conv_params,
+        second_conv_params=second_conv_params,
+        third_conv_params=third_conv_params,
+    )
+
+    # make it linear so it gets sparse afterwards
+    if add_sparsity:
+        base_conv_params["activation"] = "linear"
+
+    if add_gates:
+        resnet_params["gate_params"] = gate_params
+
+    if add_learnable_multiplier:
+        resnet_params["multiplier_params"] = multiplier_params
+
+    # --- build model
+    # set input
+    input_layer = \
+        keras.Input(
+            name="input_tensor",
+            shape=input_dims)
+    x = input_layer
+    y = input_layer
+
+    if add_var:
+        _, x_var = \
+            mean_sigma_local(
+                input_layer=x,
+                kernel_size=(5, 5))
+        x = keras.layers.Concatenate()([x, x_var])
+
+    # add base layer
+    x = keras.layers.Conv2D(**base_conv_params)(x)
+
+    if use_bn:
+        x = keras.layers.BatchNormalization(**bn_params)(x)
+
+    if add_sparsity:
+        x = \
+            sparse_block(
+                input_layer=x,
+                bn_params=None,
+                **sparse_params)
+
+    # add resnet blocks
+    x = \
+        resnet_blocks(
+            input_layer=x,
+            **resnet_params)
+
+    # optional batch norm
+    if use_bn:
+        x = keras.layers.BatchNormalization(**bn_params)(x)
+
+    # --- output layer branches here,
+    # to allow space for intermediate results
+    output_layer = x
+
+    # --- output to original channels / projection
+    if add_projection_to_input:
+        output_layer = \
+            keras.layers.Conv2D(
+                **final_conv_params)(output_layer)
+
+        # learnable multiplier
+        if add_learnable_multiplier:
+            output_layer = \
+                TrainableMultiplier(**multiplier_params)(output_layer)
+
+        # cap it off to limit values
+        output_layer = \
+            keras.layers.Activation(
+                activation=final_activation)(output_layer)
+
+    # --- skip with input layer
+    if add_skip_with_input:
+        output_layer = \
+            keras.layers.Add()([output_layer, y])
+
+    output_layer = \
+        keras.layers.Layer(name="output_tensor")(output_layer)
+
+    # return intermediate results if flag is turned on
+    output_layers = [output_layer]
+    if add_intermediate_results:
+        output_layers.append(
+            keras.layers.Layer(name="intermediate_tensor")(x))
+
+    return \
+        keras.Model(
+            name=name,
+            trainable=True,
+            inputs=input_layer,
+            outputs=output_layers)
+
+# ---------------------------------------------------------------------
