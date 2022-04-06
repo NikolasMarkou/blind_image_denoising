bee6f4a68aa789c5c06205917baccf46c979fdc5
Nikolas Markou <nikolasmarkou@gmail.com>
2022-04-06 12:18:41 +0300

    [wip] added noise mixer estimation

--- bfcnn/model_noise_estimation.py
+++ bfcnn/model_noise_estimation.py
@@ -103,9 +103,11 @@ def noise_estimation_mixer(
         x1_input_layer):
     x0_noise = model_noise_estimation(x0_input_layer)
     x1_noise = model_noise_estimation(x1_input_layer)
-    x = keras.layers.Concatenate(axis=-1)([x0_noise, x1_noise])
-    x = keras.layers.Softmax(axis=-1)(x)
+    x = keras.layers.Concatenate(axis=1)([x0_noise, x1_noise])
+    x = keras.layers.Softmax(axis=1)(x)
     x0_coeff, x1_coeff = tf.unstack(x, axis=-1)
+    x0_coeff = tf.expand_dims(x0_coeff, axis=1)
+    x1_coeff = tf.expand_dims(x1_coeff, axis=1)
     x0_result_mix = keras.layers.Multiply()([x0_input_layer, x0_coeff])
     x1_result_mix = keras.layers.Multiply()([x1_input_layer, x1_coeff])
     return keras.layers.Add()([x0_result_mix, x1_result_mix])
