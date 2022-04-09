db203ee5937f7a634206e8093df33d8de0cf3457
Nikolas Markou <nikolasmarkou@gmail.com>
2022-04-09 14:24:48 +0300

    [wip] gatenet recoding

--- bfcnn/utilities.py
+++ bfcnn/utilities.py
@@ -194,13 +194,17 @@ def step_function(
 def conv2d_wrapper(
         input_layer,
         conv_params: Dict,
-        bn_params: Dict = None):
+        bn_params: Dict = None,
+        depth_conv: bool = False):
     use_bn = bn_params is not None
     # ---
     x = input_layer
     if use_bn:
         x = keras.layers.BatchNormalization(**bn_params)(x)
-    x = keras.layers.Conv2D(**conv_params)(x)
+    if depth_conv:
+        x = keras.layers.DepthwiseConv2D(**conv_params)(x)
+    else:
+        x = keras.layers.Conv2D(**conv_params)(x)
     return x
 
 # ---------------------------------------------------------------------
@@ -637,6 +641,7 @@ def resnet_blocks(
     use_gate = gate_params is not None
     use_dropout = dropout_params is not None
     use_multiplier = multiplier_params is not None
+    mask_discount = 0.9
 
     # --- setup resnet
     x = input_layer
@@ -650,25 +655,28 @@ def resnet_blocks(
         x = conv2d_wrapper(x, conv_params=third_conv_params, bn_params=bn_params)
         # compute activation per channel
         if use_gate:
-            y = conv2d_wrapper(x, conv_params=gate_params, bn_params=bn_params)
-            y = tf.reduce_mean(y, axis=[1, 2], keepdims=True)
-            if mask is not None:
-                y = (1.0 - mask * 0.9) * y
-            y = keras.layers.Flatten()(y)
+            y = conv2d_wrapper(
+                input_layer=x,
+                conv_params=gate_params,
+                bn_params=bn_params,
+                depth_conv=True)
+            y = keras.layers.GlobalAveragePooling2D()(y)
             y = keras.layers.Dense(
                 use_bias=False,
-                activation="relu",
+                activation="linear",
                 units=third_conv_params["filters"],
                 kernel_regularizer=third_conv_params.get("kernel_regularizer", None),
                 kernel_initializer=third_conv_params.get("kernel_initializer", None))(y)
             y = tf.expand_dims(y, axis=1)
             y = tf.expand_dims(y, axis=1)
+            if mask is not None:
+                y = (1.0 - mask * mask_discount) * y
             # --- on by default, requires effort to turn off
             # if x < -2.5: return 0
             # if x > 2.5: return 1
             # if -2.5 <= x <= 2.5: return 0.2 * x + 0.5
             # mask = keras.activations.hard_sigmoid(2.5 - y)
-            mask = keras.activations.sigmoid(y)
+            mask = keras.activations.sigmoid(10*y)
             x = keras.layers.Multiply()([x, mask])
         # optional multiplier
         if use_multiplier:
@@ -839,13 +847,14 @@ def build_model_resnet(
 
     gate_params = dict(
         kernel_size=1,
-        filters=filters,
         strides=(1, 1),
         padding="same",
         use_bias=use_bias,
+        depth_multiplier=1,
         activation=activation,
         kernel_regularizer=kernel_regularizer,
-        kernel_initializer=kernel_initializer
+        kernel_initializer=kernel_initializer,
+        depthwise_initializer=kernel_initializer,
     )
 
     first_conv_params = dict(
