85b6aa02c52d3bfb2c9cfe57d4ab3e76b5ec2714
Nikolas Markou <nikolasmarkou@gmail.com>
2022-04-21 09:25:06 +0300

    [wip] restructuring pruning strategy

--- bfcnn/pruning.py
+++ bfcnn/pruning.py
@@ -21,6 +21,7 @@ from sklearn.preprocessing import StandardScaler
 
 from .custom_logger import logger
 
+
 # ---------------------------------------------------------------------
 
 
@@ -50,6 +51,11 @@ class PruneStrategy(Enum):
     """
     PCA_PROJECTION = 4
 
+    """
+    sorts the weights by absolute value and zeros out the bottom X percent
+    """
+    DROP_BOTTOM_X_PERCENT = 5
+
     @staticmethod
     def from_string(type_str: str) -> "PruneStrategy":
         # --- argument checking
@@ -66,35 +72,118 @@ class PruneStrategy(Enum):
     def to_string(self) -> str:
         return self.name
 
+
 # ---------------------------------------------------------------------
 
 
-def prune_conv2d_weights(
-        model: keras.Model,
+def prune_strategy_helper(
         strategy: PruneStrategy,
-        **kwargs) -> keras.Model:
+        **kwargs) -> Callable:
     """
-    go through the model and prune its weights given the config and strategy
-
-    :param model: model to be pruned
-    :param strategy: pruning strategy
-    :return: pruned model
+    builds the pruning function to be called
+    
+    :param strategy:
+    :param kwargs:
+    :return:
     """
+    fn = None
     if strategy == PruneStrategy.MINIMUM_THRESHOLD:
         minimum_threshold = kwargs["minimum_threshold"]
+
+        def fn(x: np.ndarray) -> np.ndarray:
+            x[np.abs(x) < minimum_threshold] = 0.0
+            return x
     elif strategy == PruneStrategy.MINIMUM_THRESHOLD_BIFURCATE:
         minimum_threshold = kwargs["minimum_threshold"]
+
+        def fn(x: np.ndarray) -> np.ndarray:
+            mask = np.abs(x) < minimum_threshold
+            rand = \
+                np.random.uniform(
+                    -minimum_threshold * 2.0,
+                    +minimum_threshold * 2.0,
+                    size=mask.shape)
+            x[mask] = rand[mask]
+            x[np.abs(x) < minimum_threshold] = 0.0
+            return x
     elif strategy == PruneStrategy.MINIMUM_THRESHOLD_SHRINKAGE:
         shrinkage = kwargs["shrinkage"]
         minimum_threshold = kwargs["minimum_threshold"]
         shrinkage_threshold = kwargs["shrinkage_threshold"]
+
+        def fn(x: np.ndarray) -> np.ndarray:
+            mask = np.abs(x) < shrinkage_threshold
+            x[mask] = x[mask] * shrinkage
+            x[np.abs(x) < minimum_threshold] = 0.0
+            return x
     elif strategy == PruneStrategy.PCA_PROJECTION:
         # required variance
         variance = kwargs["variance"]
         # optional minimum threshold
         minimum_threshold = kwargs.get("minimum_threshold", -1)
+
+        def fn(x: np.ndarray) -> np.ndarray:
+            # threshold again to zero very small values
+            if minimum_threshold != -1:
+                x[np.abs(x) < minimum_threshold] = 0.0
+            # reshape x which is 4d to 2d
+            x_transpose = np.transpose(x, axes=(3, 0, 1, 2))
+            x_transpose_shape = x_transpose.shape
+            x_reshaped = \
+                np.reshape(
+                    x_transpose,
+                    newshape=(
+                        x_transpose_shape[0],
+                        np.prod(x_transpose_shape[1:])))
+            scaler = StandardScaler()
+            scaler.fit(x_reshaped)
+            x_reshaped = scaler.transform(x_reshaped)
+            pca = PCA(n_components=variance)
+            pca.fit(x_reshaped)
+            # forward pass
+            x_reshaped = pca.transform(x_reshaped)
+            # inverse pass
+            x_reshaped = pca.inverse_transform(x_reshaped)
+            # convert back to scale
+            x_reshaped = scaler.inverse_transform(x_reshaped)
+            # reshape and transpose back to original shape
+            x_reshaped = \
+                np.reshape(
+                    x_reshaped,
+                    newshape=x_transpose_shape)
+            x = \
+                np.transpose(
+                    x_reshaped,
+                    axes=(1, 2, 3, 0))
+            return x
+    elif strategy == PruneStrategy.DROP_BOTTOM_X_PERCENT:
+        percentage = kwargs["percentage"]
+
+        def fn(x: np.ndarray) -> np.ndarray:
+            x_sorted = np.sort(np.abs(x), axis=None)
+            x_threshold = x_sorted[np.round(len(x_sorted) * percentage)]
+            x[np.abs(x) < x_threshold] = 0.0
+            return x
+    elif strategy == PruneStrategy.NONE:
+        def fn(x: np.ndarray) -> np.ndarray:
+            return x
     else:
-        pass
+        raise ValueError("invalid strategy")
+    return fn
+
+
+# ---------------------------------------------------------------------
+
+def prune_conv2d_weights(
+        model: keras.Model,
+        prune_fn: Callable) -> keras.Model:
+    """
+    go through the model and prune its weights given the config and strategy
+
+    :param model: model to be pruned
+    :param prune_fn: pruning strategy function
+    :return: pruned model
+    """
 
     for layer in model.layers:
         layer_config = layer.get_config()
@@ -103,84 +192,26 @@ def prune_conv2d_weights(
         if not layer.trainable:
             continue
         for layer_internal in layer.layers:
-            # make sure to prune only convolutions
+            # ---- make sure to prune only convolutions
             if not isinstance(layer_internal, keras.layers.Conv2D) and \
                     not isinstance(layer_internal, keras.layers.DepthwiseConv2D):
                 # skipping because not convolution
                 continue
             layer_internal_config = layer_internal.get_config()
-            # skipping because not trainable
+            # --- skipping because not trainable
             if not layer_internal_config["trainable"]:
                 continue
             # ---
             pruned_weights = []
             # get layer weights
             layer_weights = layer_internal.get_weights()
+            #  prune layer weights with the pruning strategy
+            for x in layer_weights:
+                pruned_weights.append(prune_fn(x))
             # ---
-            if strategy == PruneStrategy.NONE:
-                pruned_weights = layer_weights
-            elif strategy == PruneStrategy.MINIMUM_THRESHOLD:
-                for x in layer_weights:
-                    x[np.abs(x) < minimum_threshold] = 0.0
-                    pruned_weights.append(x)
-            elif strategy == PruneStrategy.MINIMUM_THRESHOLD_BIFURCATE:
-                for x in layer_weights:
-                    mask = np.abs(x) < minimum_threshold
-                    rand = \
-                        np.random.uniform(
-                            -minimum_threshold * 2.0,
-                            +minimum_threshold * 2.0,
-                            size=mask.shape)
-                    x[mask] = rand[mask]
-                    x[np.abs(x) < minimum_threshold] = 0.0
-                    pruned_weights.append(x)
-            elif strategy == PruneStrategy.MINIMUM_THRESHOLD_SHRINKAGE:
-                for x in layer_weights:
-                    mask = np.abs(x) < shrinkage_threshold
-                    x[mask] = x[mask] * shrinkage
-                    x[np.abs(x) < minimum_threshold] = 0.0
-                    pruned_weights.append(x)
-            elif strategy == PruneStrategy.PCA_PROJECTION:
-                for x in layer_weights:
-                    # threshold again to zero very small values
-                    if minimum_threshold != -1:
-                        x[np.abs(x) < minimum_threshold] = 0.0
-                    # reshape x which is 4d to 2d
-                    x_transpose = np.transpose(x, axes=(3, 0, 1, 2))
-                    x_transpose_shape = x_transpose.shape
-                    x_reshaped = \
-                        np.reshape(
-                            x_transpose,
-                            newshape=(
-                                x_transpose_shape[0],
-                                np.prod(x_transpose_shape[1:])))
-                    #x_reshaped = np.transpose(x_reshaped, axes=(1, 0))
-                    scaler = StandardScaler()
-                    scaler.fit(x_reshaped)
-                    x_reshaped = scaler.transform(x_reshaped)
-                    pca = PCA(n_components=variance)
-                    pca.fit(x_reshaped)
-                    # forward pass
-                    x_reshaped = pca.transform(x_reshaped)
-                    # inverse pass
-                    x_reshaped = pca.inverse_transform(x_reshaped)
-                    # convert back to scale
-                    x_reshaped = scaler.inverse_transform(x_reshaped)
-                    #x_reshaped = np.transpose(x_reshaped, axes=(1, 0))
-                    # reshape and transpose back to original shape
-                    x_reshaped = \
-                        np.reshape(
-                            x_reshaped,
-                            newshape=x_transpose_shape)
-                    x_reshaped = \
-                        np.transpose(
-                            x_reshaped,
-                            axes=(1, 2, 3, 0))
-                    pruned_weights.append(x_reshaped)
-            else:
-                raise NotImplementedError("not implemented strategy")
             layer_internal.set_weights(pruned_weights)
     return model
+
 
 # ---------------------------------------------------------------------
 
@@ -194,15 +225,16 @@ def prune_function_builder(
     """
     strategy = PruneStrategy.from_string(config["strategy"])
     strategy_config = config["config"]
+    prune_fn = prune_strategy_helper(strategy, **strategy_config)
 
     def prune(model: keras.Model) -> keras.Model:
         return \
             prune_conv2d_weights(
                 model=model,
-                strategy=strategy,
-                **strategy_config)
+                prune_fn=prune_fn)
 
     return prune
+
 
 # ---------------------------------------------------------------------
 
