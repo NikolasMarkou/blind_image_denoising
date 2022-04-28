f33bee9cb26c8d04ecd96e3e4c56416bdb4c00ea
arxwn <nikolasmarkou@gmail.com>
2022-04-28 15:22:29 +0300

    [tests] adding tests

--- tests/bfcnn/test_dataset.py
+++ tests/bfcnn/test_dataset.py
@@ -17,8 +17,8 @@ import bfcnn
 
 # ---------------------------------------------------------------------
 
-def test_dataset_builder_kitti_build():
-    config = {
+@pytest.mark.parametrize(
+    "config", [{
         "batch_size": 2,
         "min_value": 0,
         "max_value": 255,
@@ -36,15 +36,7 @@ def test_dataset_builder_kitti_build():
         "multiplicative_noise": [],
         "additional_noise": [1, 5, 10, 20, 40],
         "directory": str(KITTI_DIR)
-    }
-    dataset_results = bfcnn.dataset.dataset_builder(config=config)
-    assert "dataset" in dataset_results
-    assert "augmentation" in dataset_results
-
-# ---------------------------------------------------------------------
-
-def test_dataset_builder_megadepth_build():
-    config = {
+    }, {
         "batch_size": 2,
         "min_value": 0,
         "max_value": 255,
@@ -62,9 +54,26 @@ def test_dataset_builder_megadepth_build():
         "multiplicative_noise": [],
         "additional_noise": [1, 5, 10, 20, 40],
         "directory": str(MEGADEPTH_DIR)
-    }
+    }])
+def test_dataset_builder_build(config):
     dataset_results = bfcnn.dataset.dataset_builder(config=config)
-    assert "dataset" in dataset_results
-    assert "augmentation" in dataset_results
+    assert bfcnn.dataset.DATASET_FN_STR in dataset_results
+    assert bfcnn.dataset.AUGMENTATION_FN_STR in dataset_results
+
+    for input_batch in dataset_results[bfcnn.dataset.DATASET_FN_STR]:
+        input_batch = input_batch.numpy()
+        assert input_batch.shape[0] <= config["batch_size"]
+        assert input_batch.shape[1] == config["input_shape"][0]
+        assert input_batch.shape[2] == config["input_shape"][1]
+
+        if config["color_mode"] == "grayscale":
+            assert input_batch.shape[3] == 1
+        if config["color_mode"] == "rgb":
+            assert input_batch.shape[3] == 3
+
+        assert np.max(input_batch) <= config["max_value"]
+        assert np.min(input_batch) >= config["min_value"]
+        assert input_batch.dtype == np.float32
+
 
 # ---------------------------------------------------------------------
