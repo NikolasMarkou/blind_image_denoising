51a2220513e574c4fc9a723f3a6897057da8c8b4
arxwn <nikolasmarkou@gmail.com>
2022-04-28 12:51:42 +0300

    [tests] adding tests

--- bfcnn/__init__.py
+++ bfcnn/__init__.py
@@ -21,6 +21,8 @@ from .pyramid import \
 
 current_dir = pathlib.Path(__file__).parent.resolve()
 
+# ---------------------------------------------------------------------
+
 configs_dir = current_dir / "configs"
 
 configs = [
@@ -30,29 +32,28 @@ configs = [
 
 # ---------------------------------------------------------------------
 
+pretrained_dir = current_dir / "pretrained"
+
 pretrained_models = {}
-pretrained_dir = os.path.join(str(current_dir), "pretrained")
 
 # --- populate pretrained_models
-if os.path.exists(pretrained_dir):
+if pretrained_dir.is_dir():
     for directory in \
-            [f.path
-             for f in os.scandir(pretrained_dir) if f.is_dir()]:
+            [d for d in pretrained_dir.iterdir() if d.is_dir()]:
         # ---
-        model_name = os.path.split(directory)[-1]
+        model_name = str(directory.name)
 
         # --- define model loader function
         def load_tf():
-            return tf.keras.models.load_model(
-                os.path.join(directory, "saved_model/"))
+            return tf.keras.models.load_model(directory / "saved_model")
 
         # --- define structure for each model
         pretrained_models[model_name] = {
             "load_tf": load_tf,
             "directory": directory,
-            "tflite": os.path.join(directory, "model.tflite"),
-            "configuration": os.path.join(directory, "pipeline.json"),
-            "tf": os.path.join(directory, "saved_model/saved_model.pb")
+            "tflite": directory / "model.tflite",
+            "configuration": directory / "pipeline.json",
+            "tf": directory / "saved_model" / "saved_model.pb"
         }
 else:
     logger.info(
@@ -65,13 +66,16 @@ def load_model(model_path: str):
     # --- argument checking
     if model_path is None or len(model_path) <= 0:
         raise ValueError("model_path cannot be empty")
+
     # --- load from pretrained
     if model_path in pretrained_models:
         return pretrained_models[str(model_path)]["load_tf"]()
+
     # --- load from any directory
     if not os.path.exists(model_path):
         raise ValueError(
             "model_path [{0}] does not exist".format(model_path))
+
     return tf.keras.models.load_model(str(model_path))
 
 # ---------------------------------------------------------------------
