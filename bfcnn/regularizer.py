bce5c3f1b9e4f9464313e9499d54038edfcadcb7
Nikolas Markou <nikolasmarkou@gmail.com>
2022-04-30 17:59:33 +0300

    [regularizer] added regularizer builder

--- bfcnn/regularizer.py
+++ bfcnn/regularizer.py
@@ -28,24 +28,36 @@ class SoftOrthogonalConstraintRegularizer(keras.regularizers.Regularizer):
         self._lambda_coefficient = lambda_coefficient
 
     def __call__(self, x):
-        assert len(x.shape) == 4
-        # reshape x which is 4d to 2d
-        x_transpose = np.transpose(x, axes=(3, 0, 1, 2))
-        x_transpose_shape = x_transpose.shape
-        x_reshaped = \
-            np.reshape(
-                x_transpose,
-                newshape=(
-                    x_transpose_shape[0],
-                    np.prod(x_transpose_shape[1:])))
-        # compute Wt * W - I
-        Wt_W = \
+        # --- argument checking
+        if x is None:
+            raise ValueError("input cannot be empty")
+
+        # reshape to 2d matrix
+        if x.shape == 2:
+            x_reshaped = x
+        elif x.shape == 4:
+            # reshape x which is 4d to 2d
+            x_transpose = np.transpose(x, axes=(3, 0, 1, 2))
+            x_transpose_shape = x_transpose.shape
+            x_reshaped = \
+                np.reshape(
+                    x_transpose,
+                    newshape=(
+                        x_transpose_shape[0],
+                        np.prod(x_transpose_shape[1:])))
+        else:
+            logger.info(f"don't know how to handle shape [{x.shape}]")
+            return 0.0
+
+        # compute (Wt * W) - I
+        wt_w = \
             np.matmul(
                 np.transpose(x_reshaped, axes=(1, 0)),
                 x_reshaped)
-        I = np.identity(Wt_W.shape[0])
         # frobenius norm
-        return self._lambda_coefficient * np.linalg.cond(Wt_W - I, "fro")
+        return \
+            self._lambda_coefficient * \
+            np.linalg.cond(wt_w - np.identity(wt_w.shape[0]), "fro")
 
     def get_config(self):
         return {
