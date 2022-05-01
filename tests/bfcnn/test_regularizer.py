09060553fb7939641667908b7939cd450c382369
Nikolas Markou <nikolasmarkou@gmail.com>
2022-05-01 16:06:42 +0300

    [tests] testing regularizer

--- tests/bfcnn/test_regularizer.py
+++ tests/bfcnn/test_regularizer.py
@@ -4,8 +4,8 @@ import pytest
 import os
 import sys
 import numpy as np
+import tensorflow as tf
 from .constants import *
-
 sys.path.append(os.getcwd() + "/../")
 
 # ---------------------------------------------------------------------
@@ -18,8 +18,45 @@ from bfcnn.regularizer import SoftOrthogonalConstraintRegularizer
 # ---------------------------------------------------------------------
 
 
-def test_create_so():
-    x_random = np.random.normal(loc=0, scale=1, size=(5, 5, 3, 16))
+@pytest.mark.parametrize(
+    "shape", [
+        (5, 5, 3, 2),
+        (1, 1, 1, 4),
+        (2, 2, 2, 8),
+        (4, 4, 4, 16),
+        (8, 8, 8, 32)
+    ])
+def test_create_4d_soft_orthogonal_constraint(shape):
+    x_random = \
+        tf.random.uniform(
+            dtype=tf.float32,
+            minval=-1,
+            maxval=+1,
+            shape=shape)
+    regularizer = \
+        bfcnn.regularizer.SoftOrthogonalConstraintRegularizer(1.0)
+    result = regularizer(x_random)
+    assert result >= 0
+
+
+# ---------------------------------------------------------------------
+
+
+@pytest.mark.parametrize(
+    "shape", [
+        (3, 2),
+        (1, 4),
+        (2, 8),
+        (4, 16),
+        (8, 32)
+    ])
+def test_create_2d_soft_orthogonal_constraint(shape):
+    x_random = \
+        tf.random.uniform(
+            dtype=tf.float32,
+            minval=-1,
+            maxval=+1,
+            shape=shape)
     regularizer = \
         bfcnn.regularizer.SoftOrthogonalConstraintRegularizer(1.0)
     result = regularizer(x_random)
