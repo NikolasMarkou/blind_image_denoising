2deda8283fca648f5b8840d5b035d8f68b5b5b2d
Nikolas Markou <nikolasmarkou@gmail.com>
2022-09-30 15:40:04 +0300

    [wip] model fix

--- bfcnn/model_resnet.py
+++ bfcnn/model_resnet.py
@@ -167,7 +167,7 @@ def build_model_resnet(
         # this must be linear because it is capped later
         activation="linear",
         filters=input_dims[channel_index],
-        kernel_regularizer="l2",
+        kernel_regularizer=kernel_regularizer,
         kernel_initializer=kernel_initializer
     )
 
