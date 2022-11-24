6d9119d464322a0558b847b0b9b04865eb5b667b
Nikolas Markou <nikolasmarkou@gmail.com>
2022-11-24 16:23:34 +0200

    [wip] dataset optimization

--- bfcnn/dataset_loader.py
+++ bfcnn/dataset_loader.py
@@ -1,9 +1,12 @@
 import os
+import glob
+import pathlib
 import tensorflow as tf
 from typing import Tuple
 from keras.utils import dataset_utils
 from keras.utils import image_utils
 
+ALLOWED_FORMATS = [".bmp", ".gif", ".jpeg", ".jpg", ".png"]
 # ---------------------------------------------------------------------
 
 
@@ -38,14 +41,15 @@ def image_dataset_from_directory(
     interpolation = image_utils.get_interpolation(interpolation)
 
     def generator_fn():
-        for img_path in \
-                index_directory(
-                    directory=directory,
-                    formats=(".bmp", ".gif", ".jpeg", ".jpg", ".png"),
-                    follow_links=follow_links):
+        allowed_formats = set(ALLOWED_FORMATS)
+        for file_path in glob.iglob(pathname=os.path.join(directory, "**"), recursive=True):
+            file_path = pathlib.Path(file_path)
+            suffix = file_path.suffix.strip().lower()
+            if suffix not in allowed_formats:
+                continue
             yield \
                 load_image(
-                    path=img_path,
+                    path=str(file_path),
                     image_size=image_size,
                     num_channels=num_channels,
                     interpolation=interpolation,
@@ -90,64 +94,5 @@ def load_image(
     # img = tf.image.random_crop(img, size=random_crop)
     # return tf.cast(img, dtype=tf.uint8)
     return tf.zeros(shape=(random_crop[0], random_crop[1], num_channels), dtype=tf.uint8)
-
-# ---------------------------------------------------------------------
-
-
-def index_directory(
-        directory,
-        formats,
-        follow_links: bool = False):
-    """Make list of all files in the subdirs of `directory`
-
-    Args:
-      directory: The target directory (string).
-      formats: Allowlist of file extensions to index (e.g. ".jpg", ".txt").
-      follow_links: Whether to visits subdirectories pointed to by symlinks.
-
-    Returns:
-      tuple (file_paths, labels, class_names).
-        file_paths: list of file paths (strings).
-        labels: list of matching integer labels (same length as file_paths)
-        class_names: names of the classes corresponding to these labels, in
-          order.
-    """
-    subdirs = []
-    for subdir in sorted(tf.io.gfile.listdir(directory)):
-        if tf.io.gfile.isdir(tf.io.gfile.join(directory, subdir)):
-            if subdir.endswith("/"):
-                subdir = subdir[:-1]
-            subdirs.append(subdir)
-
-    for dirpath in (tf.io.gfile.join(directory, subdir) for subdir in subdirs):
-        for partial_filename in index_subdirectory(dirpath, follow_links, formats):
-            yield tf.io.gfile.join(directory, partial_filename)
-
-
-# ---------------------------------------------------------------------
-
-
-def index_subdirectory(directory, follow_links, formats):
-    """Recursively walks directory and list image paths and their class index.
-
-    Args:
-      directory: string, target directory.
-      follow_links: boolean, whether to recursively follow subdirectories
-        (if False, we only list top-level images in `directory`).
-      formats: Allowlist of file extensions to index (e.g. ".jpg", ".txt").
-
-    Returns:
-      tuple `(filenames, labels)`. `filenames` is a list of relative file
-        paths, and `labels` is a list of integer labels corresponding to these
-        files.
-    """
-    dirname = os.path.basename(directory)
-    valid_files = dataset_utils.iter_valid_files(directory, follow_links, formats)
-    for root, fname in valid_files:
-        absolute_path = tf.io.gfile.join(root, fname)
-        relative_path = tf.io.gfile.join(
-            dirname, os.path.relpath(absolute_path, directory)
-        )
-        yield relative_path
 
 # ---------------------------------------------------------------------
