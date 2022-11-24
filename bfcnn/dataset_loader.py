a546d33b5b6bb76b8bef05b456cc84a7c9065071
Nikolas Markou <nikolasmarkou@gmail.com>
2022-11-24 16:59:12 +0200

    [wip] dataset optimization

--- bfcnn/dataset_loader.py
+++ bfcnn/dataset_loader.py
@@ -94,7 +94,6 @@ def load_image(
     if (path is None) or (not os.path.isfile(path)):
         return tf.zeros(shape=(random_crop[0], random_crop[1], num_channels), dtype=tf.uint8)
 
-    logger.info(f"load_image: {path}")
     img = tf.io.read_file(path)
     img = tf.image.decode_image(
         img, channels=num_channels, expand_animations=False
@@ -107,6 +106,7 @@ def load_image(
     #     img = tf.image.resize(img, image_size, method=interpolation)
     img = tf.image.resize(img, image_size, method=interpolation)
     #img.set_shape((image_size[0], image_size[1], num_channels))
+    logger.info(f"load_image: {img.shape}:{path}")
     img = tf.image.random_crop(img, size=random_crop)
     img = tf.cast(img, dtype=tf.uint8)
     return img
