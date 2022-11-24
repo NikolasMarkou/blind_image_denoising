0c1820ed785e0e4c502b88f896e9bdfd3f035ab0
Nikolas Markou <nikolasmarkou@gmail.com>
2022-11-24 18:27:18 +0200

    Revert "[wip] dataset optimization"
    This reverts commit 885ebf44baebb354a777596078a62f375158cac0.

--- bfcnn/dataset_loader.py
+++ bfcnn/dataset_loader.py
@@ -244,36 +244,26 @@ def paths_and_labels_to_dataset(
 ):
     """Constructs a dataset of images and labels."""
     path_ds = tf.data.Dataset.from_tensor_slices(image_paths)
-    args = (image_size, num_channels, interpolation, crop_to_aspect_ratio, random_crop)
+    args = (num_channels, random_crop)
     return path_ds.map(
-        lambda x: load_image(x, *args),
+        map_func=lambda x: load_image(x, *args),
         num_parallel_calls=tf.data.AUTOTUNE
     )
-
 
 # ---------------------------------------------------------------------
 
 
 def load_image(
-        path, image_size, num_channels, interpolation,
-        crop_to_aspect_ratio=False,
+        path,
+        num_channels,
         random_crop: Tuple[int, int, int] = None
 ):
-    """Load an image from a path and resize it."""
+    """Load an image from a path and crop it."""
     img = tf.io.read_file(path)
     img = tf.image.decode_image(
         img, channels=num_channels, expand_animations=False
     )
-    if crop_to_aspect_ratio:
-        img = image_utils.smart_resize(
-            img, image_size, interpolation=interpolation
-        )
-    else:
-        img = tf.image.resize(img, image_size, method=interpolation)
-
-    img.set_shape((image_size[0], image_size[1], num_channels))
-    if random_crop is not None:
-        img = tf.image.random_crop(img, size=random_crop)
+    img = tf.image.random_crop(img, size=random_crop)
     return tf.cast(img, dtype=tf.uint8)
 
 # ---------------------------------------------------------------------
