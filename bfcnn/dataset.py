62b61dd668cda47b1c3765ad49ab546ddaf76783
Nikolas Markou <nikolasmarkou@gmail.com>
2022-11-29 14:36:05 +0200

    [wip] !!! EXPERIMENTAL HYDRA MIXER !!!

--- bfcnn/dataset.py
+++ bfcnn/dataset.py
@@ -9,7 +9,7 @@ from typing import Dict, Callable, Iterator, Tuple
 
 from .custom_logger import logger
 from .utilities import merge_iterators
-from .pyramid import downsample_2x2_block
+from .pyramid import gaussian_filter_layer
 from .dataset_file_operation import \
     image_filenames_dataset_from_directory_gen, \
     load_image_crop
@@ -225,12 +225,12 @@ def dataset_builder(
         jit_compile=False)
     def superres_augmentation_fn(
             input_batch: tf.Tensor) -> tf.Tensor:
-        downsampled_batch = \
-            downsample_2x2_block(
-                input_layer=input_batch,
+        d = \
+            gaussian_filter_layer(
                 kernel_size=(3, 3),
+                strides=(2, 2),
                 xy_max=(1.0, 1.0))
-        return downsampled_batch
+        return d(input_batch)
 
     # --- define noise augmentation function
     @tf.function(
