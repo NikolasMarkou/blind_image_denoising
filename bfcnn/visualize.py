f2196a25131644c22cb102083311db08222f1514
Nikolas Markou <nikolasmarkou@gmail.com>
2022-12-15 17:29:56 +0200

    [wip] fixing uncertainty

--- bfcnn/visualize.py
+++ bfcnn/visualize.py
@@ -78,7 +78,7 @@ def visualize(
     # --- output denoiser
     if denoiser_uq_batch is not None:
         denoiser_uq_batch = tf.reduce_sum(denoiser_uq_batch, axis=[3], keepdims=True)
-        denoiser_uq_batch = denoiser_uq_batch / (tf.reduce_max(denoiser_uq_batch, axis=[0, 1, 2], keepdims=True) + 0.01)
+        denoiser_uq_batch = denoiser_uq_batch / 255.0
         tf.summary.image(
             name="uncertainty/variance",
             step=global_step,
