6fad54698b96a131741631f698da4fe0185f0314
Nikolas Markou <nikolasmarkou@gmail.com>
2022-12-16 16:52:33 +0200

    [wip] added denoiser uncertainty estimation model

--- bfcnn/model_hydra.py
+++ bfcnn/model_hydra.py
@@ -441,6 +441,7 @@ def model_denoiser_builder(
 
     backbone, _, _ = model_backbone_builder(config)
     x = backbone(x)
+    x = tf.keras.layers.GaussianNoise(stddev=DEFAULT_EPSILON)(x)
 
     kernel = \
         tf.linspace(
@@ -467,13 +468,12 @@ def model_denoiser_builder(
                 channelwise_scaling=False,
                 multiplier_scaling=False)
         x_i = tf.keras.layers.GaussianNoise(stddev=DEFAULT_EPSILON)(x_i)
-        x_i_prob = \
-            tf.nn.softmax(logits=x_i, axis=3)
-        # clip small probabilities
-        x_i_prob = tf.nn.relu(x_i_prob - 1.0 / (float(uncertainty_channels) * 2))
-        # re-adjust probabilities
-        x_i_prob = \
-            x_i_prob / tf.reduce_sum(input_tensor=x_i_prob, axis=[3], keepdims=True)
+        x_i_prob = tf.nn.softmax(logits=x_i, axis=3)
+        # # clip small probabilities
+        # x_i_prob = tf.nn.relu(x_i_prob - 1.0 / (float(uncertainty_channels) * 2))
+        # # re-adjust probabilities
+        # x_i_prob = \
+        #     x_i_prob / tf.reduce_sum(input_tensor=x_i_prob, axis=[3], keepdims=True)
         x_i_expected = \
             tf.nn.conv2d(
                 input=x_i_prob,
