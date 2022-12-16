133628c11b7c2c4dd4aa3aac43801fdd31a5586b
Nikolas Markou <nikolasmarkou@gmail.com>
2022-12-17 00:55:03 +0200

    [wip] added denoiser uncertainty estimation model

--- bfcnn/model_hydra.py
+++ bfcnn/model_hydra.py
@@ -472,12 +472,12 @@ def model_denoiser_builder(
         x_i_prob = \
             x_i_prob / \
             (tf.nn.relu(tf.reduce_sum(input_tensor=x_i_prob, axis=[3], keepdims=True)) + DEFAULT_EPSILON)
-        # # clip small probabilities
-        # x_i_prob = tf.nn.relu(x_i_prob - (1.0 / float(uncertainty_channels * 10)))
-        # # re-adjust probabilities
-        # x_i_prob = \
-        #     x_i_prob / \
-        #     (tf.nn.relu(tf.reduce_sum(input_tensor=x_i_prob, axis=[3], keepdims=True)) + DEFAULT_EPSILON)
+        # clip small probabilities
+        x_i_prob = tf.nn.relu(x_i_prob - (1.0 / float(uncertainty_channels * 4)))
+        # re-adjust probabilities
+        x_i_prob = \
+            x_i_prob / \
+            (tf.nn.relu(tf.reduce_sum(input_tensor=x_i_prob, axis=[3], keepdims=True)) + DEFAULT_EPSILON)
         x_i_expected = \
             tf.nn.conv2d(
                 input=x_i_prob,
