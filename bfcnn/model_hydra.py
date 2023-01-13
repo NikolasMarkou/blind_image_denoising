[wip] added small fixed bias to softmax activation for stability

bfcnn/model_hydra.py
@@ -423,6 +423,7 @@ def model_denoiser_builder(
     lin_start = config.get("lin_start", -0.5)
     lin_stop = config.get("lin_stop", +0.5)
 
+    uncertainty_bias = config.get("uncertainty_bias", 0.0)
     uncertainty_buckets = config.get("uncertainty_buckets", 16)
     uncertainty_threshold = config.get("uncertainty_threshold", 0.0)
     uncertainty_activation = config.get("uncertainty_activation", "linear")
@@ -457,6 +458,7 @@ def model_denoiser_builder(
         expected_sigma_entropy_head(
             input_layer=x,
             conv_parameters=uncertainty_conv_params,
+            presoftmax_bias=uncertainty_bias,
             output_channels=output_channels,
             probability_threshold=uncertainty_threshold,
             linspace_start_stop=(lin_start, lin_stop))
@@ -482,102 +484,6 @@ def model_denoiser_builder(
                 x_entropy
             ],
             name=f"denoiser_head")
-
-    return model_head
-
-
-# ---------------------------------------------------------------------
-
-
-def model_superres_builder(
-        config: Dict,
-        **kwargs) -> tf.keras.Model:
-    """
-    builds the superres model on top of the backbone layer
-
-    :param config: dictionary with the superres configuration
-
-    :return: superres head model
-    """
-    # --- argument checking
-    logger.info(f"building superres model with [{config}]")
-    if kwargs:
-        logger.info(f"unused parameters [{kwargs}]")
-
-    # --- set configuration
-    use_bias = config.get("use_bias", False)
-    lin_start = config.get("lin_start", -0.5)
-    lin_stop = config.get("lin_stop", +0.5)
-    output_channels = config.get("output_channels", 3)
-    input_shape = input_shape_fixer(config.get("input_shape"))
-
-    uncertainty_buckets = config.get("uncertainty_buckets", 16)
-    uncertainty_threshold = config.get("uncertainty_threshold", 0.0)
-    uncertainty_activation = config.get("uncertainty_activation", "linear")
-    uncertainty_kernel_regularizer = config.get("uncertainty_kernel_regularizer", "l2")
-    uncertainty_kernel_initializer = config.get("uncertainty_kernel_initializer", "glorot_normal")
-    uncertainty_kernel_regularizer = regularizer_builder(uncertainty_kernel_regularizer)
-
-    # --- set network parameters
-    uncertainty_conv_params = \
-        dict(
-            kernel_size=1,
-            strides=(1, 1),
-            padding="same",
-            use_bias=use_bias,
-            filters=uncertainty_buckets,
-            activation=uncertainty_activation,
-            kernel_regularizer=uncertainty_kernel_regularizer,
-            kernel_initializer=uncertainty_kernel_initializer)
-
-    # --- define superres network here
-    model_input_layer = \
-        tf.keras.Input(
-            shape=input_shape,
-            name="input_tensor")
-    x = model_input_layer
-
-    # NOTE
-    # nearest -> conv2d (no artifacts)
-    # conv2dTranspose 3x3 (artifacts)
-    # conv2dTranspose 5x5 (artifacts)
-    # conv2d -> conv2dTranspose (artifacts)
-    x = \
-        tf.keras.layers.UpSampling2D(
-            size=(2, 2), interpolation="nearest")(x)
-
-    backbone, _, _ = model_backbone_builder(config)
-    x = backbone(x)
-
-    x_expected, x_sigma, x_entropy = \
-        expected_sigma_entropy_head(
-            input_layer=x,
-            conv_parameters=uncertainty_conv_params,
-            output_channels=output_channels,
-            probability_threshold=uncertainty_threshold,
-            linspace_start_stop=(lin_start, lin_stop))
-
-    x_expected = \
-        tf.keras.layers.Layer(
-            name="output_tensor_expected")(x_expected)
-
-    x_sigma = \
-        tf.keras.layers.Layer(
-            name="output_tensor_sigma")(x_sigma)
-
-    x_entropy = \
-        tf.keras.layers.Layer(
-            name="output_tensor_entropy")(x_entropy)
-
-    model_head = \
-        tf.keras.Model(
-            inputs=model_input_layer,
-            outputs=[
-                x_expected,
-                x_sigma,
-                x_entropy
-            ],
-            name=f"superres_head")
 
     return model_head
 

bfcnn/utilities.py
@@ -408,6 +408,7 @@ def expected_sigma_entropy_head(
         input_layer,
         conv_parameters: Union[Dict, List[Dict]],
         output_channels: int,
+        presoftmax_bias: float = 0.0,
         probability_threshold: float = 0.0,
         linspace_start_stop: Tuple[float, float] = (-0.5, +0.5)) \
             -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
@@ -417,6 +418,9 @@ def expected_sigma_entropy_head(
     :param input_layer:
     :param conv_parameters:
     :param output_channels:
+    :param presoftmax_bias:
+        small value to add before converting to probabilities
+        (helps values from becoming very very small)
     :param probability_threshold:
     :param linspace_start_stop:
 
@@ -457,9 +461,13 @@ def expected_sigma_entropy_head(
                 conv_params=params,
                 channelwise_scaling=False,
                 multiplier_scaling=False)
-        x_i = x_i_k
+
+        # add small value for arithmetic stability
+        if presoftmax_bias > 0.0:
+            x_i_k = tf.add(x_i_k, presoftmax_bias)
+
         # convert to probability
-        x_i_prob = tf.nn.softmax(x_i, axis=3)
+        x_i_prob = tf.nn.softmax(x_i_k, axis=3)
 
         # --- clip low probabilities and re-normalize
         if 0.0 < probability_threshold < 1.0: