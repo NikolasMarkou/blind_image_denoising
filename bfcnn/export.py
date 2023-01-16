f74d7682d8f3e03f492b49bd586429508ab379d9
Nikolas Markou <nikolasmarkou@gmail.com>
2023-01-16 15:57:39 +0200

    [wip] fixed export

--- bfcnn/export.py
+++ bfcnn/export.py
@@ -5,43 +5,12 @@ r"""export a bfcnn model"""
 import os
 import sys
 import argparse
-from enum import Enum
 
 # ---------------------------------------------------------------------
 # local imports
 # ---------------------------------------------------------------------
 
-from .export_model import export_model as export_model_denoise
-
-# ---------------------------------------------------------------------
-
-
-class ModelType(Enum):
-    # denoise model
-    DENOISE = 0
-
-    # decomposition model
-    DECOMPOSITION = 1
-
-    @staticmethod
-    def from_string(type_str: str) -> "ModelType":
-        # --- argument checking
-        if type_str is None:
-            raise ValueError("type_str must not be null")
-        if not isinstance(type_str, str):
-            raise ValueError("type_str must be string")
-        type_str = type_str.strip().upper()
-        if len(type_str) <= 0:
-            raise ValueError("stripped type_str must not be empty")
-
-        # --- clean string and get
-        return ModelType[type_str]
-
-    def to_string(self) -> str:
-        return self.name
-
-    def __str__(self):
-        return self.name
+from .export_model import export_model
 
 # ---------------------------------------------------------------------
 
@@ -52,16 +21,12 @@ def main(args):
         raise ValueError("Pipeline configuration [{0}] is not valid".format(
             args.pipeline_config))
 
-    if args.model_type == ModelType.DENOISE:
-        export_model_denoise(
-            pipeline_config=args.pipeline_config,
-            checkpoint_directory=args.checkpoint_directory,
-            output_directory=args.output_directory,
-            to_tflite=args.to_tflite,
-            test_model=args.test_model)
-    else:
-        raise ValueError(
-            f"don't know how to handle type [{args.model_type}]")
+    export_model(
+        pipeline_config_path=args.pipeline_config,
+        checkpoint_directory=args.checkpoint_directory,
+        output_directory=args.output_directory,
+        to_tflite=args.to_tflite,
+        test_model=args.test_model)
 
     return 0
 
@@ -71,14 +36,6 @@ def main(args):
 if __name__ == "__main__":
     # define arguments
     parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        "--model-type",
-        default=ModelType.DENOISE,
-        type=ModelType,
-        choices=list(ModelType),
-        dest="model_type",
-        help="type of model to export")
 
     parser.add_argument(
         "--pipeline-config",
