3c83f803b0db3e286e3662962f8be0c916d4d1aa
arxwn <nikolasmarkou@gmail.com>
2024-05-15 13:21:57 +0300

    [wip] cosine decay restarts

--- bfcnn/train.py
+++ bfcnn/train.py
@@ -1,5 +1,9 @@
 r"""train a bfcnn model"""
 
+__author__ = "Nikolas Markou"
+__version__ = "2.0.0"
+__license__ = "MIT"
+
 import os
 import sys
 import argparse
--- train.py
+++ train.py
@@ -36,6 +36,13 @@ def main(args):
     config = CONFIGS[model]
     config_basename = os.path.basename(config).split(".")[0]
     os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
+    """
+    0 = all messages are logged (default behavior)
+    1 = INFO messages are not printed
+    2 = INFO and WARNING messages are not printed
+    3 = INFO, WARNING, and ERROR messages are not printed
+    """
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
     run_name = args.run_name
     if run_name is None or len(run_name) <= 0:
