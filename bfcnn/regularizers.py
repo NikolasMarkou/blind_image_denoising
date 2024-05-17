0f671b294173db6c9d6f2ea82d2e0a4d78fd4abc
arxwn <nikolasmarkou@gmail.com>
2024-05-17 10:14:36 +0300

    [wip] regularizer fix

--- bfcnn/regularizers.py
+++ bfcnn/regularizers.py
@@ -4,69 +4,13 @@ blocks and builders for custom regularizers
 
 # ---------------------------------------------------------------------
 
-import numpy as np
-from enum import Enum
 import tensorflow as tf
-from tensorflow import keras
 from typing import Dict, Tuple, Union, List, Any
 
 # ---------------------------------------------------------------------
 
 from .constants import *
-from .custom_logger import logger
 
-# ---------------------------------------------------------------------
-
-
-def gaussian_kernel(
-        size: Tuple[int, int],
-        nsig: Tuple[float, float],
-        dtype: np.float64) -> np.ndarray:
-    """
-    builds a 2D Gaussian kernel array
-
-    :param size: size of of the grid
-    :param nsig: max value out of the gaussian on the xy axis
-    :param dtype: number type
-    :return: 2d gaussian grid
-    """
-    assert len(nsig) == 2
-    assert len(size) == 2
-    kern1d = [
-        np.linspace(
-            start=-np.abs(nsig[i]),
-            stop=np.abs(nsig[i]),
-            num=size[i],
-            endpoint=True,
-            dtype=dtype)
-        for i in range(2)
-    ]
-    x, y = np.meshgrid(kern1d[0], kern1d[1])
-    d = np.sqrt(x * x + y * y)
-    sigma, mu = 1.0, 0.0
-    g = np.exp(-((d - mu) ** 2 / (2.0 * (sigma ** 2))))
-    return g / g.sum()
-
-
-def gaussian_kernel_tf(
-        size: Tuple[int, int] = (3, 3),
-        nsig: Tuple[float, float] = (1.0, 1.0),
-        sigma: float = 1.0,
-        mu: float = 0.0,
-        dtype: tf.dtypes = tf.float64) -> tf.constant:
-    kern1d = [
-        tf.linspace(
-            start=-np.abs(nsig[i]),
-            stop=np.abs(nsig[i]),
-            num=size[i],
-            endpoint=True,
-            dtype=dtype)
-        for i in range(2)
-    ]
-    x, y = tf.meshgrid(kern1d[0], kern1d[1])
-    d = tf.sqrt(x * x + y * y)
-    g = tf.exp(-(tf.pow(d - mu, 2.0) / (2.0 * (tf.pow(sigma, 2.0)))))
-    return g / tf.reduce_sum(g)
 
 # ---------------------------------------------------------------------
 
@@ -93,7 +37,6 @@ def reshape_4d_to_2d(w: tf.Tensor) -> tf.Tensor:
             shape=(tf.shape(w_t)[0], -1))
 
 
-@tf.function
 def reshape_to_2d(weights: tf.Tensor) -> tf.Tensor:
     rank = len(weights.shape)
     if rank == 2:
@@ -106,7 +49,6 @@ def reshape_to_2d(weights: tf.Tensor) -> tf.Tensor:
 # ---------------------------------------------------------------------
 
 
-@tf.function
 def wt_x_w(weights: tf.Tensor) -> tf.Tensor:
     # --- reshape
     wt = reshape_to_2d(weights)
@@ -122,105 +64,8 @@ def wt_x_w(weights: tf.Tensor) -> tf.Tensor:
 
 # ---------------------------------------------------------------------
 
-
-class RegularizationType(Enum):
-    L1 = 0
-
-    L2 = 1
-
-    L1L2 = 2
-
-    ERF = 3
-
-    SOFT_ORTHONORMAL = 4
-
-    SOFT_ORTHOGONAL = 5
-
-    @staticmethod
-    def from_string(type_str: str) -> "RegularizationType":
-        # --- argument checking
-        if type_str is None:
-            raise ValueError("type_str must not be null")
-        if not isinstance(type_str, str):
-            raise ValueError("type_str must be string")
-        type_str = type_str.strip().upper()
-        if len(type_str) <= 0:
-            raise ValueError("stripped type_str must not be empty")
-
-        # --- clean string and get
-        return RegularizationType[type_str]
-
-    def to_string(self) -> str:
-        return self.name
-
-
-# ---------------------------------------------------------------------
-
-
-class SoftOrthonormalConstraintRegularizer(keras.regularizers.Regularizer):
-    """
-    Implements the soft orthogonality constraint as described in
-    Can We Gain More from Orthogonality Regularizations in Training Deep CNNs?
-    https://arxiv.org/abs/1810.09102
-
-    This constraint forces the kernels
-    to be orthogonal and have a l2 norm of 1 (orthonormal)
-    """
-
-    def __init__(self,
-                 lambda_coefficient: float = 1.0,
-                 l1_coefficient: float = 0.01,
-                 l2_coefficient: float = 0.00):
-        self._lambda_coefficient = tf.constant(lambda_coefficient, dtype=tf.float32)
-        self._l1_coefficient = tf.constant(l1_coefficient, dtype=tf.float32)
-        self._l2_coefficient = tf.constant(l2_coefficient, dtype=tf.float32)
-
-    @tf.function
-    def __call__(self, x):
-        # --- compute (Wt * W)
-        wt_w = wt_x_w(x)
-        w_shape = tf.shape(wt_w)
-        i = tf.eye(num_rows=w_shape[0])
-
-        # --- init result
-        result = tf.constant(0.0, dtype=tf.float32)
-
-        # --- frobenius norm
-        if self._lambda_coefficient > 0.0:
-            result += \
-                self._lambda_coefficient * \
-                tf.square(
-                    tf.norm(wt_w - i,
-                            ord="fro",
-                            axis=(0, 1),
-                            keepdims=False))
-
-        # --- l1 on Wt_W
-        if self._l1_coefficient > 0.0:
-            result += \
-                self._l1_coefficient * \
-                tf.reduce_sum(tf.abs(wt_w), axis=None, keepdims=False)
-
-        # --- l2 on Wt_W
-        if self._l2_coefficient > 0.0:
-            result += \
-                self._l2_coefficient * \
-                tf.reduce_sum(tf.pow(wt_w, 2.0), axis=None, keepdims=False)
-
-        return result
-
-    def get_config(self):
-        return {
-            L1_COEFFICIENT_STR: self._l1_coefficient.numpy(),
-            L2_COEFFICIENT_STR: self._l2_coefficient.numpy(),
-            LAMBDA_COEFFICIENT_STR: self._lambda_coefficient.numpy()
-        }
-
-
-# ---------------------------------------------------------------------
-
-
-class SoftOrthogonalConstraintRegularizer(keras.regularizers.Regularizer):
+@tf.keras.utils.register_keras_serializable()
+class SoftOrthogonalConstraintRegularizer(tf.keras.regularizers.Regularizer):
     """
     Implements the soft orthogonality constraint as described in
     Can We Gain More from Orthogonality Regularizations in Training Deep CNNs?
@@ -234,13 +79,15 @@ class SoftOrthogonalConstraintRegularizer(keras.regularizers.Regularizer):
     def __init__(self,
                  lambda_coefficient: float = 1.0,
                  l1_coefficient: float = 0.01,
-                 l2_coefficient: float = 0.00):
-        self._lambda_coefficient = tf.constant(lambda_coefficient, dtype=tf.float32)
-        self._l1_coefficient = tf.constant(l1_coefficient, dtype=tf.float32)
-        self._l2_coefficient = tf.constant(l2_coefficient, dtype=tf.float32)
+                 l2_coefficient: float = 0.00,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self._lambda_coefficient = lambda_coefficient
+        self._l1_coefficient = l1_coefficient
+        self._l2_coefficient = l2_coefficient
+        self._call_fn = None
 
-    @tf.function
-    def __call__(self, x):
+    def generic_fn(self, x):
         # --- compute (Wt * W)
         wt_w = wt_x_w(x)
 
@@ -275,186 +122,85 @@ class SoftOrthogonalConstraintRegularizer(keras.regularizers.Regularizer):
 
         return result
 
-    def get_config(self):
-        return {
-            L1_COEFFICIENT_STR: self._l1_coefficient.numpy(),
-            L2_COEFFICIENT_STR: self._l2_coefficient.numpy(),
-            LAMBDA_COEFFICIENT_STR: self._lambda_coefficient.numpy()
-        }
+    def __call__(self, x):
+        if self._call_fn is None:
+            self._call_fn = (
+                tf.function(func=self.generic_fn,
+                            reduce_retracing=True).get_concrete_function(x))
+        return self._call_fn(x)
 
 
 # ---------------------------------------------------------------------
 
-
-class ErfRegularizer(keras.regularizers.Regularizer):
+@tf.keras.utils.register_keras_serializable()
+class SoftOrthonormalConstraintRegularizer(tf.keras.regularizers.Regularizer):
     """
-    give incentive to expand the effective receptive field
+    Implements the soft orthogonality constraint as described in
+    Can We Gain More from Orthogonality Regularizations in Training Deep CNNs?
+    https://arxiv.org/abs/1810.09102
+
+    This constraint forces the kernels
+    to be orthogonal and have a l2 norm of 1 (orthonormal)
     """
 
     def __init__(self,
-                 l1_coefficient: float = 0.01,
+                 lambda_coefficient: float = 1.0,
+                 l1_coefficient: float = 0.001,
                  l2_coefficient: float = 0.00,
-                 nsig: Tuple[float, float] = (1.0, 1.0)):
-        self._l1_coefficient = tf.constant(l1_coefficient, dtype=tf.float32)
-        self._l2_coefficient = tf.constant(l2_coefficient, dtype=tf.float32)
-        self._nsig = nsig
+                 **kwargs):
+        super().__init__(**kwargs)
+        self._lambda_coefficient = lambda_coefficient
+        self._l1_coefficient = l1_coefficient
+        self._l2_coefficient = l2_coefficient
 
-    @tf.function
     def __call__(self, x):
-        # get kernel weights shape
-        shape = x.shape[0:2]
-
-        # for shapes of (1, 1) pass by
-        if shape[0] != 1 and shape[1] != 1:
-            # build gaussian kernel
-            gaussian_weights = \
-                tf.constant(
-                    gaussian_kernel(
-                        size=shape,
-                        nsig=self._nsig,
-                        dtype=np.float32))
-            gaussian_weights = \
-                tf.expand_dims(gaussian_weights, axis=2)
-            gaussian_weights = \
-                tf.expand_dims(gaussian_weights, axis=3)
-            # weight kernels
-            x = tf.multiply(x, gaussian_weights)
+        # --- compute (Wt * W)
+        wt_w = wt_x_w(x)
+        w_shape = tf.shape(wt_w)
+        i = tf.eye(num_rows=w_shape[0])
 
         # --- init result
         result = tf.constant(0.0, dtype=tf.float32)
 
-        # --- l1 norm
+        # --- frobenius norm
+        if self._lambda_coefficient > 0.0:
+            result += \
+                self._lambda_coefficient * \
+                tf.square(
+                    tf.norm(wt_w - i,
+                            ord="fro",
+                            axis=(0, 1),
+                            keepdims=False))
+
+        # --- l1 on Wt_W
         if self._l1_coefficient > 0.0:
             result += \
                 self._l1_coefficient * \
-                tf.reduce_sum(tf.abs(x), axis=None, keepdims=False)
+                tf.reduce_sum(tf.abs(wt_w), axis=None, keepdims=False)
 
-        # --- l2 norm
+        # --- l2 on Wt_W
         if self._l2_coefficient > 0.0:
             result += \
                 self._l2_coefficient * \
-                tf.reduce_sum(tf.pow(x, 2.0), axis=None, keepdims=False)
+                tf.reduce_sum(tf.pow(wt_w, 2.0), axis=None, keepdims=False)
 
         return result
 
-    def get_config(self):
-        return {
-            NSIG_COEFFICIENT_STR: self._nsig,
-            L1_COEFFICIENT_STR: self._l1_coefficient.numpy(),
-            L2_COEFFICIENT_STR: self._l2_coefficient.numpy()
-        }
-
 
 # ---------------------------------------------------------------------
 
-
-class RegularizerMixer(keras.regularizers.Regularizer):
+@tf.keras.utils.register_keras_serializable()
+class L1StickyRegularizer(tf.keras.regularizers.Regularizer):
     """
-    Combines regularizers
+    https://www.desmos.com/calculator/ft2si1rv8z
     """
-
     def __init__(self,
-                 regularizers: List[keras.regularizers.Regularizer]):
-        self._regularizers = regularizers
+                 l1: float = 0.1,
+                 **kwargs):
+        self.regularizer = tf.keras.regularizers.l1(l1=l1)
 
     def __call__(self, x):
-        result_regularizers = None
-
-        for regularizer in self._regularizers:
-            r = regularizer(x)
-            if result_regularizers is None:
-                result_regularizers = r
-            else:
-                result_regularizers += r
-        return result_regularizers
-
-    def get_config(self):
-        return {
-            REGULARIZERS_STR: [
-                regularizer.get_config() for
-                regularizer in self._regularizers
-            ]
-        }
-
-
-# ---------------------------------------------------------------------
-
-
-def builder_helper(
-        config: Union[str, Dict, keras.regularizers.Regularizer],
-        verbose: bool = False) -> keras.regularizers.Regularizer:
-    """
-    build a single regularizing function
-
-    :param config:
-    :param verbose: if True show extra messages
-    :return:
-    """
-    # --- argument checking
-    if config is None:
-        raise ValueError("config cannot be None")
-
-    # --- prepare variables
-    if isinstance(config, str):
-        regularizer_type = config.lower()
-        regularizer_params = {}
-    elif isinstance(config, Dict):
-        regularizer_type = config.get(TYPE_STR, None).lower()
-        regularizer_params = config.get(CONFIG_STR, {})
-    elif isinstance(config, keras.regularizers.Regularizer) \
-            and not type(config) == keras.regularizers.Regularizer:
-        return config
-    else:
-        raise ValueError("don't know how to handle config")
-
-    # --- logging
-    if verbose:
-        logger.info(f"building configuration with config [{config}")
-
-    # --- select correct regularizer
-    regularizer = None
-    regularizer_type = RegularizationType.from_string(regularizer_type)
-    if regularizer_type == RegularizationType.L1:
-        regularizer = keras.regularizers.L1(**regularizer_params)
-    elif regularizer_type == RegularizationType.L2:
-        regularizer = keras.regularizers.L2(**regularizer_params)
-    elif regularizer_type == RegularizationType.ERF:
-        regularizer = ErfRegularizer(**regularizer_params)
-    elif regularizer_type == RegularizationType.L1L2:
-        regularizer = keras.regularizers.L1L2(**regularizer_params)
-    elif regularizer_type == RegularizationType.SOFT_ORTHONORMAL:
-        regularizer = SoftOrthonormalConstraintRegularizer(**regularizer_params)
-    elif regularizer_type == RegularizationType.SOFT_ORTHOGONAL:
-        regularizer = SoftOrthogonalConstraintRegularizer(**regularizer_params)
-    else:
-        raise ValueError(f"don't know how to handle [{regularizer_type}]")
-    return regularizer
-
-
-# ---------------------------------------------------------------------
-
-
-def builder(
-        config: Union[str, Dict, List]) -> keras.regularizers.Regularizer:
-    """
-    build a single or mixed regularization function
-
-    :param config:
-    :return:
-    """
-    # --- argument checking
-    if config is None:
-        raise ValueError("config cannot be None")
-
-    # --- prepare variables
-    if isinstance(config, List):
-        regularizers = [
-            builder_helper(config=r) for r in config
-        ]
-    else:
-        return builder_helper(config=config)
-
-    # --- mixes all the regularizes together
-    return RegularizerMixer(regularizers=regularizers)
+        x_composite = tf.math.abs(-tf.math.abs(x+0.5) + 0.5)
+        return self.regularizer(x_composite)
 
 # ---------------------------------------------------------------------
