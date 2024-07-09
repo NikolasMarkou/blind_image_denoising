51a8a310ba2bce27a644bbb66b8c92085bcaa230
arxwn <nikolasmarkou@gmail.com>
2024-07-09 15:56:58 +0300

    [wip] small architecture change

--- bfcnn/backbone_unet_laplacian.py
+++ bfcnn/backbone_unet_laplacian.py
@@ -589,7 +589,7 @@ def builder(
                 x = tf.keras.layers.BatchNormalization(center=use_bias)(x)
             if use_ln:
                 x = tf.keras.layers.LayerNormalization(center=use_bias)(x)
-        x = activation_wrapper(activation=activation(x))
+        x = activation_wrapper(activation=activation)(x)
         output_layers[i] = (
             tf.keras.layers.Layer(
                 name=f"{output_layer_name}_{i}")(x))
