43277e1b580f29891a76733e1948f36f97262b1b
arxwn <nikolasmarkou@gmail.com>
2024-07-10 16:23:12 +0300

    [wip] small architecture change

--- bfcnn/backbone_unet_laplacian.py
+++ bfcnn/backbone_unet_laplacian.py
@@ -358,8 +358,8 @@ def builder(
             if use_ln:
                 x = tf.keras.layers.LayerNormalization(center=use_bias)(x)
 
-        if d != (depth - 1):
-            x = activation_wrapper(activation=activation)(x)
+        # if d != (depth - 1):
+        #     x = activation_wrapper(activation=activation)(x)
 
         node_level = (d, 0)
         nodes_visited.add(node_level)
