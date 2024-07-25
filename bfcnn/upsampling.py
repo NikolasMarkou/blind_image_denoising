a79e0e7bb3dd1670695f182f70ffcdd0d08b297b
arxwn <nikolasmarkou@gmail.com>
2024-07-25 12:27:42 +0300

    [wip] new subtle change

--- bfcnn/upsampling.py
+++ bfcnn/upsampling.py
@@ -81,17 +81,16 @@ def upsample(
                 lambda_coefficient=DEFAULT_SOFTORTHONORMAL_LAMBDA,
                 l1_coefficient=DEFAULT_SOFTORTHONORMAL_L1,
                 l2_coefficient=DEFAULT_SOFTORTHONORMAL_L2)
-        # lower level, upscale
-        x = \
-            tf.keras.layers.UpSampling2D(
-                size=(2, 2),
-                interpolation="nearest")(x)
         x = conv2d_wrapper(
             input_layer=x,
             bn_params=bn_params,
             ln_params=ln_params,
             conv_params=params,
             conv_type=ConvType.CONV2D)
+        x = \
+            tf.keras.layers.UpSampling2D(
+                size=(2, 2),
+                interpolation="nearest")(x)
     elif upsample_type in ["upsample_laplacian_conv2d"]:
         # specialized for laplacian network
         params["kernel_size"] = (1, 1)
