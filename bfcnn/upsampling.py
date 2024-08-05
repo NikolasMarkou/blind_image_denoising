bc0452949d5015d62b7d834245f275388b3950ee
arxwn <nikolasmarkou@gmail.com>
2024-08-05 11:30:42 +0300

    [wip] small optimization

--- bfcnn/upsampling.py
+++ bfcnn/upsampling.py
@@ -91,6 +91,25 @@ def upsample(
             tf.keras.layers.UpSampling2D(
                 size=(2, 2),
                 interpolation="nearest")(x)
+    elif upsample_type in ["upsample_bilinear_conv2d_v1"]:
+        params["kernel_size"] = (1, 1)
+        params["strides"] = (1, 1)
+        params["padding"] = "same"
+        params["kernel_regularizer"] = \
+            SoftOrthonormalConstraintRegularizer(
+                lambda_coefficient=DEFAULT_SOFTORTHONORMAL_LAMBDA,
+                l1_coefficient=DEFAULT_SOFTORTHONORMAL_L1,
+                l2_coefficient=DEFAULT_SOFTORTHONORMAL_L2)
+        x = conv2d_wrapper(
+            input_layer=x,
+            bn_params=bn_params,
+            ln_params=ln_params,
+            conv_params=params,
+            conv_type=ConvType.CONV2D)
+        x = \
+            tf.keras.layers.UpSampling2D(
+                size=(2, 2),
+                interpolation="bilinear")(x)
     elif upsample_type in ["upsample_laplacian_conv2d"]:
         # specialized for laplacian network
         params["kernel_size"] = (1, 1)
